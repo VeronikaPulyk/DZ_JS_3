145ec63f5e2520e3b5642614268937987e0cae9d
Veronika Pulyk <veronica.pulyk.kpi@gmail.com>
2017-12-02 18:49:20 +0200

    2

--- js.js
+++ js.js
@@ -1,44 +1,49 @@
 // 1
+var r = [1, 2, 3, 4, 'five', 6, 'seven'];
 document.getElementById('step1').onclick = f1;
 function f1 (){
-var r = ['red', 'green', 'blue'];
-var p = document.getElementById('new');
-p.innerHTML = r;
+	var p = document.getElementById('new');
+	var s = '';
+	for (var i = 0; i<r.length; i++) {
+		if (r[i] != undefined) {
+			s += r[i] + ', ';
+		} else {
+			s += ', ';
+		}
+		p.innerHTML = r;
+	}
 }
+f1();
 
 // 2
 document.getElementById('step2').onclick = f2;
 function f2 (){
-var color = ['red', 'green', 'blue'];
-var i = document.getElementById('input1').value;
-var r = document.getElementById('input2').value;
-var p = document.getElementById('new');
-p.innerHTML = alert(i+'='+r);
+	var input1 = parseInt(document.getElementById('input1').value);
+	var input2 = document.getElementById('input2').value;
+	r[input1] = input2;
+	f1();
 }
 
 // 3
 document.getElementById('pop').onclick = f3;
 function f3 (){
-var r = ['red', 'green', 'blue'];
-r.pop();
-var p = document.getElementById('new');
-p.innerHTML = r;
-}
+	r.pop();
+	f1();
+	}
 
 // 4
 document.getElementById('shift').onclick = f4;
 function f4 (){
-var r = ['red', 'green', 'blue'];
+
 r.shift();
-var p = document.getElementById('new');
-p.innerHTML = r;
+	f1();
+
 }
 
 // 5
 document.getElementById('push').onclick = f5;
 function f5 (){
-var r = ['red', 'green', 'blue'];
 r.push("white");
-var p = document.getElementById('new');
-p.innerHTML = r;
+	f1();
+
 }
